25ba3f13b9fecfe8229c7eae06291f24bad7d030
plcsilveira <plcarvalho2010@hotmail.com>
2022-02-09 18:07:08 -0300

    first commit

---
+++ TELA DE LOGIN/login.js
@@ -0,0 +1,23 @@
+
+let dadosusuariononistema = {
+    usersis: 'plcsilveira',
+    senhasis: '123456',
+    acessosis: 'Funcionário'
+}
+
+function entrar(){
+    let user = document.getElementById('login').value
+    let senha = document.getElementById('senha').value
+    let acesso = document.querySelector('input[name="nivelacesso"]:checked').value
+    let dadosusuario = {
+        luser: user,
+        lsenha: senha,
+        lacesso: acesso
+    }
+    if(dadosusuario.luser == dadosusuariononistema.usersis && dadosusuario.lsenha == dadosusuariononistema.senhasis && dadosusuario.lacesso == dadosusuariononistema.acessosis){
+        window.alert("Acesso permitido")
+    }else{
+        window.alert("Acesso negado")
+    }
+}
+
