09d1685f687c66d72d95c5edfaf146642af5f356
plcsilveira <plcarvalho2010@hotmail.com>
2022-02-14 13:28:43 -0300

    .

--- SISTEMA-DE-EMPRÉSTIMO-DE-EQUIPAMENTOS/sistema.js
+++ SISTEMA-DE-EMPRÉSTIMO-DE-EQUIPAMENTOS/sistema.js
@@ -1,23 +1,24 @@
 
-let dadosusuariononistema = {
-    usersis: 'plcsilveira',
+let dadosusuariononistema = []
+dadosusuariononistema = {
+    usersis: 'Pedro',
     senhasis: '123456',
     acessosis: 'Funcionário'
 }
+dadosusuariononistema[1].usersis="Lucas"
+dadosusuariononistema[1].senhasis="123456"
+dadosusuariononistema[1].aessosis="Administrador"
 
 function entrar(){
     let user = document.getElementById('inputLogin').value
     let senha = document.getElementById('inputSenha').value
     let acesso = document.querySelector('input[name="nivelacesso"]:checked').value
-    let dadosusuario = {
-        luser: user,
-        lsenha: senha,
-        lacesso: acesso
-    }
-    if(dadosusuario.luser == dadosusuariononistema.usersis && dadosusuario.lsenha == dadosusuariononistema.senhasis && dadosusuario.lacesso == dadosusuariononistema.acessosis){
+
+    if(user == dadosusuariononistema.usersis && senha == dadosusuariononistema.senhasis && acesso == dadosusuariononistema.acessosis){
         window.alert("Acesso permitido")
         if(acesso == 'Funcionário'){
             window.alert("Você está logado como "+acesso)
+            window.location.href="telainicialfun.html"
         }
     }else{
         window.alert("Verifique seus dados e tente novamente!")
