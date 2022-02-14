04ea6c067205a33e8803a5e7a19ab4aaa2259261
plcsilveira <plcarvalho2010@hotmail.com>
2022-02-14 18:51:55 -0300

    .

--- SISTEMA-DE-EMPRÉSTIMO-DE-EQUIPAMENTOS/sistema.js
+++ SISTEMA-DE-EMPRÉSTIMO-DE-EQUIPAMENTOS/sistema.js
@@ -1,28 +1,77 @@
-
-let dadosusuariononistema = []
-dadosusuariononistema = {
-    usersis: 'Pedro',
-    senhasis: '123456',
-    acessosis: 'Funcionário'
-}
-dadosusuariononistema[1].usersis="Lucas"
-dadosusuariononistema[1].senhasis="123456"
-dadosusuariononistema[1].aessosis="Administrador"
-
+//VARIAVÉIS PARA ARMAZENAR DADOS DO USUÁRIO NO SISTEMA
+let listaUser = []
+listaUser.push(
+    {
+        login: "Pedro",
+        senha: "123456",
+        acesso: "Funcionário" 
+    }
+)
+listaUser.push(
+    {
+        login: "Lucas",
+        senha: "000123",
+        acesso: "Administrador"
+    }
+)
+listaUser.push(
+    {
+        login: "Maria",
+        senha: "000456",
+        acesso: "Usuário"
+    }
+)
+listaUser.push(
+    {
+        login: "Joao",
+        senha: "000789",
+        acesso: "Chefe de Setor"
+    }
+)
+// INICIO FUNÇÃO DO BOTÃO DO FORMULÁRIO DE LOGIN
 function entrar(){
-    let user = document.getElementById('inputLogin').value
+    let validado = true
+    //VARIÁVEIS PARA ARMAZENAR INFORMAÇÕES DOS INPUT'S DA TELA DE LOGIN
+    let login = document.getElementById('inputLogin').value
     let senha = document.getElementById('inputSenha').value
     let acesso = document.querySelector('input[name="nivelacesso"]:checked').value
 
-    if(user == dadosusuariononistema.usersis && senha == dadosusuariononistema.senhasis && acesso == dadosusuariononistema.acessosis){
-        window.alert("Acesso permitido")
-        if(acesso == 'Funcionário'){
-            window.alert("Você está logado como "+acesso)
-            window.location.href="telainicialfun.html"
-        }
+    //VALIDAÇÃO CAMPO VAZIO
+    if(login == "" || senha == ""){
+        window.alert("Verifique se os campos foram preenchidos corretamente!")
     }else{
-        window.alert("Verifique seus dados e tente novamente!")
-        
+        listaUser.forEach((item) => {
+            if(login == item.login && senha == item.senha && acesso == item.acesso){
+                validado = true
+            }else{
+                validado = false
+            }
+        })
+        if(validado){
+            window.alert("Acesso permitido")
+            switch(acesso) {
+                case 'Funcionário':
+                    window.alert("Você está logado como "+acesso)
+                    window.location.href = "telainicialfun.html";
+                    break;
+                case 'Usuário':
+                    window.alert("Você está logado como "+acesso)
+                    window.location.href = "telainicialuser.html";
+                    break;
+                case 'Administrador':
+                    window.alert("Você está logado como "+acesso)
+                    window.location.href = "telainicialadm.html";
+                    break;
+                case 'Chefe de Setor':
+                    window.alert("Você está logado como "+acesso)
+                    window.location.href = "telainicialchefe.html";
+                    break;
+            }
+        }
+        else{
+            window.alert("Verifique suas informações e tente novamente!")
+            window.alert("Informações: "+login+" "+senha+" "+acesso)
+        }
     }
-}
 
+}// FIM FUNÇÃO DO BOTÃO DO FORMULÁRIO DE LOGIN
