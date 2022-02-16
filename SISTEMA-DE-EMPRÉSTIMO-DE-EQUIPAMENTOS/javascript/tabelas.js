648ea41bd5a2c0b40318a8a71ca709c837d8b41f
plcsilveira <plcarvalho2010@hotmail.com>
2022-02-16 18:36:50 -0300

    .

---
+++ SISTEMA-DE-EMPRÉSTIMO-DE-EQUIPAMENTOS/javascript/tabelas.js
@@ -0,0 +1,22 @@
+
+'use strict';
+const tabelaEquipamentosADM = document.querySelector('div#tabelaEquipamentos');
+const tabelaFuncionariosADM = document.querySelector('div#tabelaFuncionarios');
+const tabelaUsuariosADM = document.querySelector('div#tabelaUsuarios');
+const tabelaEquipamentosFunc = document.querySelector('div#gerenciaEquipamentos');
+
+const inserirLTEADM = function(){
+    alert(`inserirLTEADM is working`);
+}
+
+const inserirLTUADM = function(){
+    alert(`inserirLTUADM is working`);
+}
+
+const inserirLTFADM = function(){
+    alert(`inserirLTFADM is working`);
+}
+
+const inserirLTEFunc = function(){
+    alert(`inserirLTEFunc is working`);
+}
